188607909f01abc3e40084727e20b0ab0f5d88bb
manasi <manasibharati94@gmail.com>
2024-09-19 17:40:49 +0530

    commited

--- DSML_PBL/app.py
+++ DSML_PBL/app.py
@@ -1,5 +1,7 @@
 import secrets
 import os
+import pickle
+import numpy as np
 from flask import Flask, render_template, redirect, request, session, url_for, flash,jsonify
 from pymongo import MongoClient
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin,current_user
@@ -112,6 +114,44 @@ def logout():
     logout_user()
     flash('You have been logged out.', 'success')
     return redirect(url_for('index'))   
+
+
+# Load the scaler, label encoder, model, and class names=====================
+scaler = pickle.load(open("Models/scaler.pkl", 'rb'))
+model = pickle.load(open("Models/model.pkl", 'rb'))
+class_names = ['Lawyer', 'Doctor', 'Government Officer', 'Artist', 'Unknown',
+               'Software Engineer', 'Teacher', 'Business Owner', 'Scientist',
+               'Banker', 'Writer', 'Accountant', 'Designer',
+               'Construction Engineer', 'Game Developer', 'Stock Investor',
+               'Real Estate Developer']
+
+# Recommendations ===========================================================
+def Recommendations(gender, part_time_job, absence_days, extracurricular_activities,
+                    weekly_self_study_hours, math_score, history_score, physics_score,
+                    chemistry_score, biology_score, english_score, geography_score,
+                    total_score, average_score):
+    # Encode categorical variables
+    gender_encoded = 1 if gender.lower() == 'female' else 0
+    part_time_job_encoded = 1 if part_time_job else 0
+    extracurricular_activities_encoded = 1 if extracurricular_activities else 0
+
+    # Create feature array
+    feature_array = np.array([[gender_encoded, part_time_job_encoded, absence_days, extracurricular_activities_encoded,
+                               weekly_self_study_hours, math_score, history_score, physics_score,
+                               chemistry_score, biology_score, english_score, geography_score, total_score,
+                               average_score]])
+
+    # Scale features
+    scaled_features = scaler.transform(feature_array)
+
+    # Predict using the model
+    probabilities = model.predict_proba(scaled_features)
+
+    # Get top five predicted classes along with their probabilities
+    top_classes_idx = np.argsort(-probabilities[0])[:3]
+    top_classes_names_probs = [(class_names[idx], probabilities[0][idx]) for idx in top_classes_idx]
+
+    return top_classes_names_probs
 
 
 
